a92d8e80a2eb2ffc8c8b0693c8ba998a92f18822
yusiang <10656010@ntub.edu.tw>
2020-05-12 14:54:18 +0800

    完成作業

--- MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,43 +11,53 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textView,textViewResult;
-int a,l,r,password;
-private Button button ;
 
-
+    private Button ab,gcd,range,prime ;
+    private Button[] buttons={range,prime};
+Class[] content={RangeActivity.class,PrimeActivity.class};
+int [] parms={R.id.range,R.id.prime};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.editText);
-        textViewResult=findViewById(R.id.textView);
-        button=findViewById(R.id.button);
-        Random random=new Random();
-        l=1;
-        r=99;
-        password=random.nextInt(97);
-        password+=2;
-        button.setOnClickListener(new View.OnClickListener() {
+       range=findViewById(R.id.range);
+       prime=findViewById(R.id.prime);
+       ab=findViewById(R.id.ab);
+       gcd=findViewById(R.id.gcd);
+       range.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent=new Intent(MainActivity.this,RangeActivity.class);
+               startActivity(intent);
+               finish();
+           }
+       });
+       gcd.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent=new Intent(MainActivity.this,GcdActivity.class);
+               startActivity(intent);
+               finish();
+           }
+       });
+        prime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               int a=Integer.parseInt(textView.getText().toString());
-
-
-
-                if(a<password)
-                    l=a;
-                else r=a;
-
-                String result=("please enter in range of "+l+" and "+r);
-                if(a==password){
-                    result="You win";
-                }
-                textViewResult.setText(result);
-
+                Intent intent=new Intent(MainActivity.this,PrimeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        ab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,AbActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
+
+
 
 }
--- MyApplication4/app/src/main/java/com/example/myapplication/PrimeActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/PrimeActivity.java
@@ -1,47 +1,56 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Random;
 
 public class PrimeActivity extends AppCompatActivity {
-private TextView textView,textViewResult;
-int a,l,r,password;
-private Button button ;
+    private TextView textView, textViewResult;
+
+    private Button button, back;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime);
-        textView=findViewById(R.id.editText);
-        textViewResult=findViewById(R.id.textView);
-        button=findViewById(R.id.button);
-        Random random=new Random();
-        l=1;
-        r=99;
-        password=random.nextInt(97);
-        password+=2;
+        textView = findViewById(R.id.editText);
+        textViewResult = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PrimeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               int a=Integer.parseInt(textView.getText().toString());
-
-
-
-                if(a<password)
-                    l=a;
-                else r=a;
-
-                String result=("please enter in range of "+l+" and "+r);
-                if(a==password){
-                    result="You win";
+                int i = Integer.parseInt(textView.getText().toString());
+                int count = 0;
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        count++;
+                    }
+                    if (count > 2) {
+                        break;
+                    }
                 }
-                textViewResult.setText(result);
+                if (count == 2) {
+                   textViewResult.setText(i + " is prime");
+                } else {
+                    textViewResult.setText(i + " is not prime");
+                }
 
             }
         });
